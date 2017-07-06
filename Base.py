cce5204e3325661192a8f734c9ce689740ec10f0
alumno <benjathje@hotmail.com>
2017-07-06 11:57:41 -0300

    Casi termino lo del archivo

--- Base.py
+++ Base.py
@@ -2,7 +2,19 @@ import pymysql
 from Persona import Person
 
 class List (object):
-    Lista = []
+    Lista = None
+
+    db = None
+    c = None
+
+    def __init__ (self):
+        self.db = pymysql.connect(host="localhost", user='root', passwd='', db='mydb', autocommit=True)
+        self.c = self.db.cursor(pymysql.cursors.DictCursor)
+        self.Lista = []
+
+    def Load (self):
+        with open("Archivo.txt", "r") as Archive:
+
 
     def Print(self):
         for item in self.Lista:
@@ -12,16 +24,20 @@ class List (object):
 
     def Insert(self, person):
         self.Lista.append(person)
-        db = pymysql.connect(host="localhost", user='root', passwd='', db='test', autocommit=True)
-        c = db.cursor(pymysql.cursors.DictCursor)
-        c.execute("insert into Persona values('" + person.ID + "', '" + person.Name + "', '" + person.Surname + "')")
+        self.c.execute("insert into Persona values('" + person.ID + "', '" + person.Name + "', '" + person.Surname + "')")
 
     def Delete(self, person):
-        db = pymysql.connect(host="localhost", user='root', passwd='', db='test', autocommit=True)
-        c = db.cursor(pymysql.cursors.DictCursor)
-        c.execute("delete from Persona where DNI = '" + person.ID + "';")
+        self.c.execute("delete from Persona where DNI = '" + person + "';")
+        for item in self.Lista:
+            if item.ID == person:
+                self.Lista.remove(item)
 
     def Update(self, person):
-        db = pymysql.connect(host="localhost", user='root', passwd='', db='test', autocommit=True)
-        c = db.cursor(pymysql.cursors.DictCursor)
-        c.execute("update Persona set Nombre = '" + person.Name + "', Apellido= '" + person.Surname + "' where DNI = '" + person.ID + "';")
+        self.c.execute("update Persona set Nombre = '" + person.Name + "', Apellido= '" + person.Surname + "' where DNI = '" + person.ID + "';")
+
+    def Test (self):
+        with open("Archivo.txt", "a") as Archive:
+            for item in self.Lista:
+                Archive.write(item.ID + "|")
+                Archive.write(item.Name + "|")
+                Archive.write(item.Surname + "| \n")
--- File1.py
+++ File1.py
@@ -2,25 +2,28 @@ import pymysql
 from Persona import Person
 from Base import List
 
-Persona1 = Person()
 Lista1 = List()
 
+Lista1.Load()
+
 while True:
+
     Input = input("1 - Add Person\n"
                   "2 - Delete Person\n"
                   "3 - Update Person\n"
                   "4 - Print List\n"
-                  "5 - Exit\n")
+                  "5 - Write in Archive\n"
+                  "6 - Exit\n")
 
     if Input == "1":
+        Persona1 = Person()
         Persona1.setID(input("ID "))
         Persona1.setName(input("Name "))
         Persona1.setSurname(input("Surname "))
         Lista1.Insert(Persona1)
 
     elif Input == "2":
-        Persona1.setID (input("ID "))
-        Lista1.Delete(Persona1)
+        Lista1.Delete(input("ID "))
 
     elif Input == "3":
         Persona1.setID (input("ID "))
@@ -32,4 +35,7 @@ while True:
         Lista1.Print()
 
     elif Input == "5":
+        Lista1.Test()
+
+    elif Input == "6":
         exit()
